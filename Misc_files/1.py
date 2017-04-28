36ad324af42a2b8b7dd077441f4d1451f72e22c5
Jay Vala <jay.vala@msn.com>
2017-04-28 16:47:58 +0200

    Update 1.py

--- Misc_files/1.py
+++ Misc_files/1.py
@@ -1,79 +1,72 @@
-from __future__ import division, print_function, absolute_import
 
-import timeit
-import numpy as np
-import tflearn
-import pandas as pd
-import tensorflow as tf
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import classification_report
-import sys
-import os
-from random import randint
+# Parameters
+learning_rate = 0.01
+training_epochs = 20
+batch_size = 256
+display_step = 1
+examples_to_show = 10
 
+# Network Parameters
+n_hidden_1 = 41 # 1st layer num features
+n_hidden_2 = 30 # 2nd layer num features
+n_hidden_3 = 20 # 3st layer num features
+n_hidden_4 = 10 # 4st layer num features
+n_hidden_5 = 5  # 5st layer num features, we will use this for classification.
 
-os.system('clear')
-#timer 
-start = timeit.default_timer()
-#Placeholder for the trainer
+n_input = 41
 
-f = randint(0,20)
+#defining a pretraining_network using tensorflow
+with tf.Graph().as_default():
+	#model variables 
+	x = tf.placeholder("float",[None, 41])
+	y = tf.placeholder("float",[None, 5])
 
-#dataset
-df = pd.read_csv('/Project/incomplete_project/autoencoder/Kdd_Test_41.csv') 			# test set 
-er = pd.read_csv('/Project/incomplete_project/autoencoder/NSL_TestLabels_mat5.csv') 	# test labels
-ad = pd.read_csv('/Project/incomplete_project/autoencoder/Kdd_Train_41.csv') 			# train set 
-qw = pd.read_csv('/Project/incomplete_project/autoencoder/NSL_TrainLabels_mat5.csv') 	# train labels
-tr = pd.read_csv('/Project/incomplete_project/autoencoder/Kdd_Valid_41.csv')			# valid set
-yu = pd.read_csv('/Project/incomplete_project/autoencoder/NSL_ValidLabels_int3.csv')    # valid labels
-rt = pd.read_csv('/Project/incomplete_project/autoencoder/NSL_TrainLabels_int.csv')
-a = df.values
-b = ad.values
-c = qw.values
-d = er.values
-e = tr.values
-f = yu.values
-g = rt.values
-test_set = np.float32(a)
-train_set = np.float32(b)
-train_labels_set = np.float32(c)
-valid_labels_set = np.float32(d)
-valid_set = np.float32(e)
-test_labels_set = np.float32(f)
-test_set_for_CM =np.float32(g)
-x = tf.placeholder("float",[None, 41])
-y = tf.placeholder("float",[None, 41])
-print("pretraining a network")
-# First Layer
-encoder_layer = tflearn.fully_connected(x,41,activation='relu',bias=True,restore=True)
-encoder_layer = tflearn.fully_connected(x,30,activation='relu',bias=True,restore=True)
-encoder_layer = tflearn.fully_connected(x,20,activation='relu',bias=True,restore=True)
-encoder_layer = tflearn.fully_connected(x,10,activation='relu',bias=True,restore=True)
-encoder_layer = tflearn.fully_connected(x,5,activation='softmax',bias=True,restore=True)
+	weights = {
+		'encoder_h1': tf.Variable(tf.random_normal([n_input, n_hidden_1]),name="layer_1_weights"),
+		'encoder_h2': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2])name="layer_2_weights"),
+		'encoder_h3': tf.Variable(tf.random_normal([n_hidden_2, n_hidden_3])name="layer_3_weights"),
+		'encoder_h4': tf.Variable(tf.random_normal([n_hidden_3, n_hidden_4])name="layer_4_weights"),
+		'encoder_h5': tf.Variable(tf.random_normal([n_hidden_4, n_hidden_5])name="layer_5_weights"),
+	}
+	biases = {
+		'encoder_b1': tf.Variable(tf.random_normal([n_hidden_1])name="layer_1_bias"),
+		'encoder_b2': tf.Variable(tf.random_normal([n_hidden_2])name="layer_2_bias"),
+		'encoder_b3': tf.Variable(tf.random_normal([n_hidden_3])name="layer_3_bias"),
+		'encoder_b4': tf.Variable(tf.random_normal([n_hidden_4])name="layer_4_bias"),
+		'encoder_b5': tf.Variable(tf.random_normal([n_hidden_5])name="layer_5_bias"),
+	}
 
-loss = tflearn.losses.L2(encoder_layer, wd = 0.001)
-
-#optimizer function which will try and minimize the loss function
-
-sgd = tflearn.optimizers.SGD(learning_rate = 0.01,name ='SGD').get_tensor()
-
-# we will now define training operations we want to perform on the data
-
-train_ops = tflearn.helpers.trainer.TrainOp(loss,sgd, batch_size = 64)
-#Now we will define the trainer function which will take the train_op and create a network
-
-#this is our model
-trainer = tflearn.helpers.trainer.Trainer(train_ops,tensorboard_verbose=0)
-
-#we will use fit method to train the network with our data.
-trainer.fit(feed_dicts={x:train_set},n_epoch=1, show_metric=False, run_id='firstlayer_pretraining')
-print("Saving weights and biases...") 
-trainer.save('model_pretraning')
-
-
-
-#Now training the model
-print("Traing and fine tuning the model...")
-trainer1.restore('model_pretraning')
-trainer1.fit(feed_dicts={x:train_set,y:train_labels_set},n_epoch=1, show_metric=True, run_id='firstlayer_pretraining')
-
+	#For pretraining we will create a network
+	def pretraining_network(x):
+		layer_1 = tf.nn.sigmoid(tf.add(tf.matmul(x, weights['encoder_h1']),biases['encoder_b1'])) #first layer will use sigmoid activation
+		layer_2 = tf.nn.sigmoid(tf.add(tf.matmul(x, weights['encoder_h2']),biases['encoder_b2'])) #second layer will use sigmoid activation
+		layer_3 = tf.nn.sigmoid(tf.add(tf.matmul(x, weights['encoder_h3']),biases['encoder_b3'])) #third layer will use sigmoid activation
+		layer_4 = tf.nn.sigmoid(tf.add(tf.matmul(x, weights['encoder_h4']),biases['encoder_b4'])) #fourth layer will use sigmoid activation
+		layer_5 = tf.nn.softmax(tf.add(tf.matmul(x, weights['encoder_h5']),biases['encoder_b5'])) #fifth layer will use sigmoid activation
+		return layer_5
+		
+	net = pretraining_network(x)
+	loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=net, labels=y))
+	optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.1)
+	
+	
+	#We will use tflearn trainer_op fucntion to define all the pretrain prarameters for the pretraining
+	
+	pretrain_op = tflearn.TrainOp(loss=loss, optimizer=optimizer,batch_size=100)
+	
+	#Now we will create a trainer to train the network
+	
+	trainer = tflearn.Trainer(train_ops=pretrain_op,tensorboard_verbose=0)
+	
+	#Now here we will train the model
+	
+	trainer.fit({x:test_set},n_epoch=1,show_metric=True)
+	
+	#Invoking tf.saver to save weights and biases
+	
+	saver = tf.train.Saver()
+	save_path = saver.save("/testfiles/dnn.ckpt")
+	print("saving the models weights and biases")
+	
+	
+		
